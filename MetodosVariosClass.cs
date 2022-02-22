018c78930ad62d8e1cedaca3b7a35287ec0db5e0
luisnrq <luisenrique.frias@tajamar365.com>
2022-02-22 11:53:53 +0100

    Add project files.

---
+++ MetodosVariosClass.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaPrimerServicioWCF
+{
+    public class MetodosVariosClass : IMetodosVariosContract
+    {
+        public List<int> GetTablaMultiplicar(int numero)
+        {
+            List<int> results = new List<int>();
+            for(int i = 1; i<= 10; i++)
+            {
+                int op = numero * i;
+                results.Add(op);
+            }
+            return results;
+        }
+
+        public int GetNumeroDoble(int numero)
+        {
+            return numero * 2;
+        }
+
+        public string GetSaludo(string nombre)
+        {
+            return "Mi primer WCF de martes, " + nombre;
+        }
+
+        public string MetodoInvisible()
+        {
+            return "no valgo para el servicio";
+        }
+    }
+}
